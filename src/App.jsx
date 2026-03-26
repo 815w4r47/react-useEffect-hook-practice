de934495c02dced5fe4c85d4f6d3b305d4eddd9c
815w4r47 <majumderbiswaraj1997@gmail.com>
2026-03-26 23:39:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import HookCounterOne from "./components/HookCounterOne.jsx";
+import HookMouse from "./components/HookMouse.jsx";
+import MouseContainer from "./components/MouseContainer.jsx";
+import IntervalHookCounter from "./components/IntervalHookCounter.jsx";
+
+function App() {
+  return (
+    <>
+      {/* <HookCounterOne /> */}
+      {/* <HookMouse /> */}
+      {/* <MouseContainer /> */}
+      <IntervalHookCounter />
+    </>
+  );
+}
+
+export default App;
